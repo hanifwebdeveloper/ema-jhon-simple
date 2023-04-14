9f7d57c56bf4cb0b82feb524251af7a0cb1d5049
MD HANIF PATWARY <hanifwebdeveloper@gmail.com>
2023-04-14 16:38:50 +0600

    retrieve product from stored cart state

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -17,8 +17,23 @@ const Shop = () => {
 
     useEffect(() => {
         const storedCart = getShoppingCart();
-        console.log(storedCart);
-    }, [])
+        const savedCart = [];
+        // step 1: get id of the addedProduct
+        for (const id in storedCart) {
+            // get product from products state by using id 
+            const addedProduct = products.find(product => product.id === id)
+            if (addedProduct) {
+                // step 3: add quantity
+                const quantity = storedCart[id]
+                addedProduct.quantity = quantity;
+                // step 4: add the addedproduct to the saved cart
+                savedCart.push(addedProduct);
+            }
+            console.log('addede product', addedProduct);
+        }
+        // step 5: set the cart
+        setCart(savedCart);
+    }, [products])
 
 
     const handleAddToCart = (product) => {
