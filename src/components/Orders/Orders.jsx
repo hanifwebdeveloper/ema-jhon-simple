9fafd53136c1463d3803f64c6fcc19bd6f4da35c
MD HANIF PATWARY <hanifwebdeveloper@gmail.com>
2023-05-06 22:31:44 +0600

    implement from cart

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Cart from '../Cart/Cart';
 import { useLoaderData } from 'react-router-dom';
 import './Orders.css';
 import ReviewItem from '../ReviewItem/ReviewItem';
+import { removeFromDb } from '../utilities/fakedb';
 
 
 const Orders = () => {
-    const cart = useLoaderData();
+    const savedCart = useLoaderData();
+    const [cart, setCart] = useState(savedCart);
 
-    console.log(cart);
+    const handleRemovedFromCart = (id) => {
+        const remaining = cart.filter(product => product.id !== id);
+        setCart(remaining);
+        removeFromDb(id);
+    }
+
     return (
         <div className='shop-container'>
             <div className='review-container'>
@@ -16,6 +23,7 @@ const Orders = () => {
                     cart.map(product => <ReviewItem
                         key={product.id}
                         product={product}
+                        handleRemovedFromCart={handleRemovedFromCart}
                     ></ReviewItem>)
                 }
             </div>
